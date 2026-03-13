9d5a49ae4b41bae6740666ff44fedfab79797308
Yefry Barreto <yohanbarreto48@gmail.com>
2026-03-12 22:54:27 -0500

    Agregar Dockerfile para deploy en Cloud Run

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,28 @@
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT || 3306),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = pool;
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
