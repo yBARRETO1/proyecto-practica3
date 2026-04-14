090865b658f7d955332309964fa2ad6a9715c3ea
Yefry Barreto <yohanbarreto48@gmail.com>
2026-04-13 22:17:08 -0500

    fix:  ssl false

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: undefined,
+
   ...(isCloud
     ? {
         socketPath: process.env.DB_HOST
@@ -15,9 +15,7 @@ const pool = mysql.createPool({
         host: process.env.DB_HOST,
         port: process.env.DB_PORT
       }),
-
-  // 🔥 SOLUCIÓN REAL
-  ssl: isCloud ? false : false,
-  enableKeepAlive: true,
-  connectTimeout: 10000
+      
 });
+
+module.exports = pool;
